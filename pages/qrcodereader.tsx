ce93ff838ad237f872b88b88de84cada5e406d88
osoken <osoken.devel@outlook.jp>
2023-03-21 10:22:20 +0900

    feat(qrcodereader): add error interaction

--- pages/qrcodereader.tsx
+++ pages/qrcodereader.tsx
@@ -6,7 +6,7 @@ import QrCodeReaderButton from "@/components/QrCodeReaderButton";
 import PointVoucherDisplay from "@/components/PointVoucherDisplay";
 import API from "@/src/api";
 import { Voucher } from "@/types/Voucher";
-import { TextField, CircularProgress, Alert } from "@mui/material"
+import { TextField, CircularProgress, Alert, Collapse } from "@mui/material"
 
 
 const Home: NextPage<{ liff: Liff | null; liffError: string | null }> = ({
@@ -15,14 +15,26 @@ const Home: NextPage<{ liff: Liff | null; liffError: string | null }> = ({
 }) => {
   const [pointVoucherData, setPointVoucherData] = useState<Voucher | null>(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [idValue, setIdValue] = useState("");
+  const [isInvalid, setIsInvalid] = useState(false);
   const [done, setDone] = useState(false);
+  const showErrorMessage = () => {
+    setIsInvalid(true);
+    setTimeout(() => {setIsInvalid(false)}, 5000)
+  }
   const onGetIdCallback = (value: string | null) => {
+    setIdValue(value || "");
     setIsLoading(true);
     if (value) {
       API.getVoucher(value).then((res) => {
         setPointVoucherData(res);
-      }).finally(() => {setIsLoading(false)});
+      }).catch(
+        (err) => {
+          showErrorMessage();
+        }
+      ).finally(() => {setIsLoading(false)});
     } else {
+      showErrorMessage();
       setPointVoucherData(null);
       setIsLoading(false);
     }
@@ -45,9 +57,10 @@ const Home: NextPage<{ liff: Liff | null; liffError: string | null }> = ({
 
   return (
     <main className={styles.main}>
-      {done && <Alert severity="success">ポイントを受け取りました！</Alert>}
+      <Collapse in={done}><Alert severity="success">ポイントを受け取りました！</Alert></Collapse> 
+      <Collapse in={isInvalid}><Alert severity="error">ID が無効です</Alert></Collapse> 
       {liff && <QrCodeReaderButton liff={liff} callback={onGetIdCallback} />}
-      <TextField label="直接 ID を入力する"  onChange={(e) => onGetIdCallback(e.target.value)} />
+      <TextField label="直接 ID を入力する" value={idValue} onChange={(e) => onGetIdCallback(e.target.value)} />
       {isLoading && <CircularProgress />}
       {pointVoucherData && <PointVoucherDisplay open={!!pointVoucherData} data={pointVoucherData} onConfirm={onConfirm} onCancel={onCancel} />}
     </main>
